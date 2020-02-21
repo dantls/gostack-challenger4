d728a01b8ff8817dc8c7f99ec54123cd5697fe1d
dantls <dan_tls@hotmail.com>
2020-02-21 02:25:38 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './App.css';
+
+import PostList from './components/PostList/PostList';
+import Header from './components/Header';
+
+function App()
+{
+ return (
+    <>
+    <Header/>
+    <PostList/>   
+    </>
+ )
+ 
+}
+
+export default App;
